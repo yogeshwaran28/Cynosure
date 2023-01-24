0241c101ff9ce10b4a0555e9ffe533a5cec1cd1e
yogeshwaran28 <123474079+yogeshwaran28@users.noreply.github.com>
2023-01-24 06:42:38 -0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let a = document.getElementsByClassName('toggle-button')[0];
+let b = document.getElementsByClassName('link')[0];
+
+a.addEventListener("click",toggle);
+
+function toggle() {
+	b.classList.toggle('active');
+}
