870a1ec6ba07674054655ac06990dbb99dbdb0c5
yogesh <yogeshwaran.2002@srit.org>
2023-01-24 21:48:23 -0900

    image

--- index.js
+++ index.js
@@ -1,9 +1,59 @@
 let a = document.getElementsByClassName('toggle-button')[0];
 let b = document.getElementsByClassName('link')[0];
-
-a.addEventListener("click",toggle);
+ a.addEventListener("click",toggle);
 
 function toggle() {
-	b.classList.toggle('active');
+ 	b.classList.toggle('active');
+	 }
+
+
+const menuBtn = 
+
+document.querySelector(".toggle-button");
+
+
+const menu = 
+
+document.querySelector(".navbar");
+
+
+
+
+let showMenu = false;
+
+
+menuBtn.addEventListener("click", toggleMenu);
+
+
+function toggleMenu() {
+
+if (!showMenu) {
+
+	menuBtn.classList.toggle("close");
+
+	menu.classList.toggle("show");
+
 	
+
+	// Reset the menu state
+
+	showMenu = true;
+
+} else {
+
+	menuBtn.classList.remove("close");
+
+	menu.classList.remove("show");
+
+	
+
+
+	// Reset the menu state
+
+	showMenu = false;
+
 }
+
+}
+
+
