64bf7a38a4fe4c24b38ac09911d43c439adc880d
yogesh <yogeshwaran.2002@srit.org>
2023-01-26 04:06:00 -0900

    clock

--- index.js
+++ index.js
@@ -56,4 +56,22 @@ if (!showMenu) {
 
 }
 
+var deadline = new Date("feb 8, 2023 12:00:00").getTime();
+var x = setInterval(function() 
+{
+var now = new Date().getTime();
+var t = deadline - now;
+var days = Math.floor(t / (1000 * 60 * 60 * 24));
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((t % (1000 * 60)) / 1000);
+document.getElementById("demo").innerHTML = days + "d "
++ hours + "h " + minutes + "m " + seconds + "s ";
+	if (t < 0) {
+		clearInterval(x);
+		document.getElementById("demo").innerHTML = "EXPIRED";
+	}
+}, 1000);
+
+
 
