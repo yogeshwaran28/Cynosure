f8043e9e30c22da2b028a7c527be1edc512ef0da
yogesh <yogeshwaran.2002@srit.org>
2023-01-27 15:13:48 -0900

    update

--- index.js
+++ index.js
@@ -36,24 +36,6 @@ function toggleMenu() {
 
 }
 
-var deadline = new Date("feb 8, 2023 12:00:00").getTime();
-
-var x = setInterval(function() 
-{
-var now = new Date().getTime();
-var t = deadline - now;
-var days = Math.floor(t / (1000 * 60 * 60 * 24));
-var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((t % (1000 * 60)) / 1000);
-document.getElementById("demo").innerHTML = days + "d "
-+ hours + "h " + minutes + "m " + seconds + "s ";
-	if (t < 0) {
-		clearInterval(x);
-		document.getElementById("demo").innerHTML = "EXPIRED";
-	}
-}, 1000);
-
 
 
 
@@ -66,41 +48,65 @@ document.getElementById("demo").innerHTML = days + "d "
 	}
 
 	 $(document).ready(function () {
-	 	$(window).scroll(function () {
-		var scroll = $(window).scrollTop();
- 		if (scroll > 100) {
-	 			$(".menubar").css("background", "#fff")
+ 	 	$(window).scroll(function () {
+ 		var scroll = $(window).scrollTop();
+  		if (scroll > 100) {
+ 	 			$(".menubar").css("background", "#fff")
 			
 				 
-		}
+ 		}
 
-		else {
- 	    	$(".menubar").css("background", "transparent")
-	 		}
+ 		else {
+  	    	$(".menubar").css("background", "transparent")
+ 	 		}
 	 	})
-	})
+ 	})
   
-var x = setInterval(function () {
-	var now = new Date().getTime();
-	var t = deadline - now;
-	var days = Math.floor(t / (1000 * 60 * 60 * 24));
-	var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-	var seconds = Math.floor((t % (1000 * 60)) / 1000);
-	document.getElementById("demo").innerHTML = days + "d "
-		+ hours + "h " + minutes + "m " + seconds + "s ";
-	var x = setInterval(function () {
-		var now = new Date().getTime();
-		var t = deadline - now;
-		var days = Math.floor(t / (1000 * 60 * 60 * 24));
-		var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-		var seconds = Math.floor((t % (1000 * 60)) / 1000);
-		document.getElementById("demo").innerHTML = days + "d "
-			+ hours + "h " + minutes + "m " + seconds + "s ";
-		if (t < 0) {
-			clearInterval(x);
-			document.getElementById("demo").innerHTML = "EXPIRED";
-		}
-	}, 1000);
-})
+// var x = setInterval(function () {
+// 	var now = new Date().getTime();
+// 	var t = deadline - now;
+// 	var days = Math.floor(t / (1000 * 60 * 60 * 24));
+// 	var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+// 	var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+// 	var seconds = Math.floor((t % (1000 * 60)) / 1000);
+// 	document.getElementById("demo").innerHTML = days + "d "
+// 		+ hours + "h " + minutes + "m " + seconds + "s ";
+// 	var x = setInterval(function () {
+// 		var now = new Date().getTime();
+// 		var t = deadline - now;
+// 		var days = Math.floor(t / (1000 * 60 * 60 * 24));
+// 		var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+// 		var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+// 		var seconds = Math.floor((t % (1000 * 60)) / 1000);
+// 		document.getElementById("demo").innerHTML = days + "  "
+// 			+ hours + "  " + minutes + "  " + seconds + "  ";
+// 		if (t < 0) {
+// 			clearInterval(x);
+// 			document.getElementById("demo").innerHTML = "EXPIRED";
+// 		}
+// 	}, 1000);
+// })
+
+// counting days to new year
+const countDownDateTime = new Date(2023, 1, 8, 12, 0, 0).getTime();
+const daysValue = document.querySelector("#days");
+const hoursValue = document.querySelector("#hours");
+const minutesValue = document.querySelector("#minutes");
+const secondsValue = document.querySelector("#seconds");
+// run this function every 1000 ms or 1 second
+let x = setInterval(function () {
+ const dateTimeNow = new Date().getTime();
+ let difference = countDownDateTime - dateTimeNow;
+ // calculating time and assigning values
+ daysValue.innerHTML = Math.floor(difference / (1000 * 60 * 60 * 24));
+ hoursValue.innerHTML = Math.floor(
+ (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+ );
+ minutesValue.innerHTML = Math.floor(
+ (difference % (1000 * 60 * 60)) / (1000 * 60)
+ );
+ secondsValue.innerHTML = Math.floor((difference % (1000 * 60)) / 1000);
+ if (difference < 0) {
+ clearInterval(x);
+ }
+}, 1000);
