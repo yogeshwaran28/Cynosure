47fd87ca06eef50eb500ea89a980af5fa8ad0c0c
yogesh <yogeshwaran.2002@srit.org>
2023-01-28 16:56:29 -0900

    update commt

--- index.js
+++ index.js
@@ -51,30 +51,6 @@ function toggleMenu() {
 	 	})
  	})
   
-// var x = setInterval(function () {
-// 	var now = new Date().getTime();
-// 	var t = deadline - now;
-// 	var days = Math.floor(t / (1000 * 60 * 60 * 24));
-// 	var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-// 	var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-// 	var seconds = Math.floor((t % (1000 * 60)) / 1000);
-// 	document.getElementById("demo").innerHTML = days + "d "
-// 		+ hours + "h " + minutes + "m " + seconds + "s ";
-// 	var x = setInterval(function () {
-// 		var now = new Date().getTime();
-// 		var t = deadline - now;
-// 		var days = Math.floor(t / (1000 * 60 * 60 * 24));
-// 		var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-// 		var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-// 		var seconds = Math.floor((t % (1000 * 60)) / 1000);
-// 		document.getElementById("demo").innerHTML = days + "  "
-// 			+ hours + "  " + minutes + "  " + seconds + "  ";
-// 		if (t < 0) {
-// 			clearInterval(x);
-// 			document.getElementById("demo").innerHTML = "EXPIRED";
-// 		}
-// 	}, 1000);
-// })
 
 // counting days to new year
 const countDownDateTime = new Date(2023, 1, 8, 12, 0, 0).getTime();
